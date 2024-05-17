6fc1b2c291ddb5dea34c337987ee730235efa276
Eider Agudelo <eider841agudelo@hotmail.com>
2024-05-16 22:11:18 -0400

    Se crea la validación del formulario

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,67 @@
 <script>
-  export default {
-    data() {
-      return {
-        gravedades: ["Baja", "Media", "Alta"],
+export default {
+  data() {
+    return {
+      gravedades: ["Baja", "Media", "Alta"],
+      pacientes: [],
+      datos: {
         pacienteTx: "",
         fecha: "",
-        hora: "",        
+        hora: "",
         gravedad: "",
-        motivoTx: "",
-        conContenido: false,
-        sinContenido: true
+        motivoTx: ""
+      },
+      conContenido: {
+        pacienteTx: false,
+        fecha: false,
+        hora: false,
+        gravedad: false,
+        motivoTx: false
       }
-    },
-    methods: {
-      textoRojo(){
-        if (this.pacienteTx != '') {
-          this.conContenido = true
-          this.sinContenido = false
-        }
-        if (this.fecha != '') {
-          this.conContenido = true
-          this.sinContenido = false
-        }
-        if (this.hora != '') {
-          this.conContenido = true
-          this.sinContenido = false
-        }
-        if (this.motivoTx != '') {
-          this.conContenido = true
-          this.sinContenido = false
-        }
+    }
+  },
+  methods: {
+    textoRojo(index) {
+      if (this.datos[index] != '') {
+        this.conContenido[index] = true
+      } else {
+        this.conContenido[index] = false
       }
-    },
-  }
+    }
+  },
+}
 </script>
 
 <template>
   <div class="container">
     <div class="row mt-5">
       <div class="col-sm-2 mb-3 text-center">
-        <label for="paciente" class="form-label text-center" :class="{conContenido: conContenido, sinContenido: sinContenido}">Paciente</label>
-        <input type="text" class="form-control" id="paciente" v-model="pacienteTx" @change="textoRojo"/>
+        <label for="paciente" class="form-label text-center"
+          :class="{ sinContenido: !conContenido.pacienteTx }">Paciente</label>
+        <input type="text" class="form-control" id="paciente" v-model="datos.pacienteTx" @input="textoRojo('pacienteTx')" />
       </div>
       <div class="col-sm-2 mb-3 text-center">
-        <label for="fecha" class="form-label text-center" :class="{conContenido: conContenido, sinContenido: !conContenido}">Fecha</label>
-        <input type="date" class="form-control" id="fecha" v-model="fecha" @change="textoRojo"/>
+        <label for="fecha" class="form-label text-center"
+          :class="{ sinContenido: !conContenido.fecha }">Fecha</label>
+        <input type="date" class="form-control" id="fecha" v-model="datos.fecha" @input="textoRojo('fecha')" />
       </div>
       <div class="col-sm-2 mb-3 text-center">
-        <label for="hora" class="form-label text-center" :class="{conContenido: conContenido, sinContenido: !conContenido}">Hora</label>
-        <input type="time" class="form-control" id="hora" v-model="hora" @change="textoRojo"/>
+        <label for="hora" class="form-label text-center"
+          :class="{ sinContenido: !conContenido.hora }">Hora</label>
+        <input type="time" class="form-control" id="hora" v-model="datos.hora" @input="textoRojo('hora')" />
       </div>
       <div class="col-sm-3 mb-3 text-center">
-        <label class="form-label text-center" :class="{conContenido: conContenido, sinContenido: !conContenido}">Gravedad</label>
-        <select class="form-select" aria-label="Default select example" v-model="gravedad">
+        <label class="form-label text-center"
+          :class="{ sinContenido: !conContenido.gravedad }">Gravedad</label>
+        <select class="form-select" aria-label="Default select example" v-model="datos.gravedad" @change="textoRojo('gravedad')">
           <option selected>Seleccione gravedad del paciente</option>
-          <option v-for="(gravedad, i) in gravedades" :value=gravedad :key="i">{{gravedad}}</option>
+          <option v-for="(gravedad, i) in gravedades" :value=gravedad :key="i">{{ gravedad }}</option>
         </select>
       </div>
       <div class="col-sm-3 mb-3 text-center">
-        <label for="motivo" class="form-label text-center" :class="{conContenido: conContenido, sinContenido: !conContenido}">Motivo</label>
-        <textarea class="form-control" id="motivo" rows="1" v-model="motivoTx" @change="textoRojo"></textarea>
+        <label for="motivo" class="form-label text-center"
+          :class="{ sinContenido: !conContenido.motivoTx }">Motivo</label>
+        <textarea class="form-control" id="motivo" rows="1" v-model="datos.motivoTx" @input="textoRojo('motivoTx')"></textarea>
       </div>
     </div>
     <div class="row">
@@ -68,20 +70,16 @@
       </div>
     </div>
 
-    <p>{{ pacienteTx }}</p>
-    <p>{{ fecha }}</p>
-    <p>{{ hora }}</p>
-    <p>{{ gravedad }}</p>
-    <p>{{ motivoTx }}</p>
+    <p>{{ datos.pacienteTx }}</p>
+    <p>{{ datos.fecha }}</p>
+    <p>{{ datos.hora }}</p>
+    <p>{{ datos.gravedad }}</p>
+    <p>{{ datos.motivoTx }}</p>
   </div>
 </template>
 
 <style>
-  .conContenido {
-    color: #000;
-  }
-
-  .sinContenido {
-    color: red;
-  }
+.sinContenido {
+  color: red;
+}
 </style>
